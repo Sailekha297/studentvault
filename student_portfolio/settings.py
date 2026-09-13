e05a38c077284314b462aa6ef35f918795894f09
Sai <sailekha083@gmail.com>
2026-09-13 15:26:35 +0530

    Configure Render CSRF trusted origin

--- student_portfolio/settings.py
+++ student_portfolio/settings.py
@@ -7,6 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'change-this-in-production'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://studentvault-hoj0.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
